896f3c81282a738b2768d9353ad75749b0f8fa3d
Stephen Zhou <hi@hyoban.cc>
2024-05-07 21:46:26 +0800

    fix: only full name

--- docs/scripts/snippets.ts
+++ docs/scripts/snippets.ts
@@ -2,27 +2,12 @@ import { writeFileSync } from 'node:fs'
 import { builtinCommands } from '../../src/commands'
 
 const commandNames = builtinCommands.map(command => command.name)
-const commandNamesAbbr = commandNames.map(name => name.split('-').map(word => word[0]).join(''))
-
-const fullBody = `/// \$\{1|${commandNames.join(',')}|}`
-const atBody = `// @\$\{1|${commandNames.join(',')}|}`
-const abbrBody = `/// \$\{1|${commandNamesAbbr.join(',')}|}`
-
+const body = `/// \$\{1|${commandNames.join(',')}|}`
 const snippets = {
-  'eslint-plugin-command-full': {
+  'eslint-plugin-command': {
     prefix: 'epc',
-    body: fullBody,
+    body,
     description: 'ESLint plugin command',
-  },
-  'eslint-plugin-command-abbr': {
-    prefix: 'epc',
-    body: abbrBody,
-    description: 'ESLint plugin command (abbr)',
-  },
-  'eslint-plugin-command-at': {
-    prefix: 'epc',
-    body: atBody,
-    description: 'ESLint plugin command (@)',
   },
 }
 
